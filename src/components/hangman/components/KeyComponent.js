4e44b3b6055f0c1fb8bb4fe6ff38a3d4ea983aca
GracielaJhovanaCayoCondori <gracielacayo12345@gmail.com>
2021-09-17 23:24:12 -0400

    laboratorio3

---
+++ src/components/hangman/components/KeyComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const KeyComponent = ({letter, handlerKey, status}) => {
+    return (
+    <div
+    className= {status ? "disablediv": ("")} 
+     onClick={() => {
+        handlerKey(letter);
+    }}
+    >
+    {letter}
+    </div>
+    );
+};
+
+
